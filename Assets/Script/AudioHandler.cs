3ea68451c1025011a77423f01bec473a30129869
LAPTOP-B5EI7JRQ\AloneSurvival <harsh.hbsb@gmail.com>
2026-02-24 14:15:26 +0530

    Car flip logic implement top of that sound logic with sound add and implement and add

--- Assets/Script/AudioHandler.cs
+++ Assets/Script/AudioHandler.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class AudioHandler : MonoBehaviour
 {
+    public static AudioHandler Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance != null & Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private AudioSource audioSource;
+    private AudioClip[] audioClips;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaySound(int id, float volume = 1f)
     {
-        
+        audioSource.PlayOneShot(audioClips[id], volume);
     }
 }
--- Assets/Script/Card.cs
+++ Assets/Script/Card.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private int cardID;
+    private int id;
+    private bool isFlipped;
+    private bool turning;
+
+
+    private IEnumerator CardFlip(Transform thisTransform, float time, bool changeSprite)
     {
-        
+        Quaternion startRotation = thisTransform.rotation;
+        Quaternion endRotation = thisTransform.rotation * Quaternion.Euler(new Vector3(0, 90, 0));
+        float rate = 1.0f / time;
+        float t = 0.0f;
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime * rate;
+            thisTransform.rotation = Quaternion.Slerp(startRotation, endRotation, t);
+
+            yield return null;
+        }
+        //change sprite and flip another 90degree
+        if (changeSprite)
+        {
+            isFlipped = !isFlipped;
+          //      ChangeSprite();
+            StartCoroutine(CardFlip(transform, time, false));
+        }
+        else
+            turning = false;
+    }
+    // perform a 180 degree flip
+    public void Flip()
+    {
+        turning = true;
+      //  AudioPlayer.Instance.PlayAudio(0);
+        StartCoroutine(CardFlip(transform, 0.25f, true));
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CardBtnEvt()
     {
-        
+        if (isFlipped || turning) return;
+     //   if (!_CardGameManager.Instance.canClick()) return;
+        Flip();
+        StartCoroutine(SelectionEvent());
+    }
+
+    private IEnumerator SelectionEvent()
+    {
+        yield return new WaitForSeconds(0.5f);
+       // _CardGameManager.Instance.cardClicked(spriteID, id);
     }
 }
